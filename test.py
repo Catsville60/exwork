e08ce1339fcf0c7782e87cca045eef9afb58fddf
Catsville60 <sanrav3000@gmail.com>
2019-03-25 17:33:42 +0700

    Переделал под RDAP

--- test.py
+++ test.py
@@ -1,1 +1,65 @@
+# -*- coding: utf-8 -*-
+import sys
+import socket
+import xlrd
+from ipwhois import IPWhois
+import ipwhois
+from pprint import pprint
+from PyQt5.QtWidgets import QWidget, QPushButton, QApplication
+from PyQt5.QtCore import QCoreApplication
 
+
+rb = xlrd.open_workbook('C:/Users/Пользователь/Desktop/Статистика. Васильченко. 21-22.03.2019.xls', formatting_info=True)
+sheet = rb.sheet_by_index(0)
+vals = [sheet.row_values(rownum) for rownum in range(sheet.nrows)]
+
+r_count = sheet.nrows
+print('Row count: ', r_count)
+
+x = 0
+y = 27
+x1 = 1
+y1 = 0
+
+while y <= r_count:
+    try:
+        try:
+
+            ais = socket.gethostbyaddr((vals[y])[x])
+
+            w = IPWhois((vals[y])[x])
+            res = w.lookup_rdap()
+            resp = res.get('objects')
+            resp1 = list(resp.keys())
+            resp2 = resp1[0]
+            resp3 = resp.get(resp2)
+            resp4 = resp3.get('contact')
+            resp5 = resp4.get('name')
+
+            print('Host: ', ais[0], ' Owner: ', resp5)
+
+            y = y + 1
+
+        except socket.herror:
+
+            w = IPWhois((vals[y])[x])
+            ress = w.lookup_rdap()
+            respp = ress.get('objects')
+            respp1 = list(respp.keys())
+            respp2 = respp1[0]
+            respp3 = respp.get(respp2)
+            respp4 = respp3.get('contact')
+            respp5 = respp4.get('name')
+
+            print('Host: Not found! Owner: ', respp5)
+
+
+            y = y + 1
+
+    except ipwhois.exceptions.IPDefinedError as e:
+
+        err = '%s' % e
+
+        print(' - ', err)
+
+        y = y + 1
--- testexel.py
+++ testexel.py
@@ -3,10 +3,13 @@ import sys
 import socket
 import xlrd
 from ipwhois import IPWhois
+import ipwhois
+import pprint
 from PyQt5.QtWidgets import QWidget, QPushButton, QApplication
 from PyQt5.QtCore import QCoreApplication
 
-rb = xlrd.open_workbook('c:/Users/Пользователь/PycharmProjects/untitled1/tet.xls', formatting_info=True)
+
+rb = xlrd.open_workbook('C:/Users/Пользователь/Desktop/Статистика. Васильченко. 21-22.03.2019.xls', formatting_info=True)
 sheet = rb.sheet_by_index(0)
 vals = [sheet.row_values(rownum) for rownum in range(sheet.nrows)]
 
@@ -14,34 +17,37 @@ r_count = sheet.nrows
 print('Row count: ', r_count)
 
 x = 0
-y = 11
+y = 27
 x1 = 1
 y1 = 0
-class test(IPWhois):
-    def whoisid(self):
-        while y <= r_count:
-            try:
-                ais = socket.gethostbyaddr((vals[y])[x])
 
-                w = IPWhois((vals[y])[x])
-                res = w.lookup_whois()
-                resp = res.get('nets')
-                resp1 = resp[0]
-                resp2 = resp1.get('description')
+while y <= r_count:
+    try:
+        try:
+            socket.setdefaulttimeout(1)
+            ais = socket.gethostbyaddr((vals[y])[x])
 
-                print('Host: ', ais[0], ' Owner: ', resp2)
+            w = IPWhois((vals[y])[x])
+            res = w.lookup_whois()
+            resp = res.get('nets')
+            resp1 = resp[0]
+            resp2 = resp1.get('description')
 
-                y = y + 1
-            except socket.herror:
-                w = IPWhois((vals[y])[x])
-                res = w.lookup_whois()
-                resp = res.get('nets')
-                resp1 = resp[0]
-                resp2 = resp1.get('description')
+            print('Host: ', ais[0], ' Owner: ', resp2)
 
-                print('Host: Not found! Owner: ', resp2)
+            y = y + 1
+        except socket.herror:
+            w = IPWhois((vals[y])[x])
+            res = w.lookup_whois()
+            resp = res.get('nets')
+            resp1 = resp[0]
+            resp2 = resp1.get('description')
 
-                y = y + 1
+            print('Host: Not found! Owner: ', resp2)
 
-
+            y = y + 1
+    except ipwhois.exceptions.IPDefinedError as e:
+        err = '%s' % e
+        print(' - ', err)
+        y = y + 1
 
