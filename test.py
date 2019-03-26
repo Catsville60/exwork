23780f14391bfa4bcb9e95474e0d8b932b056c3f
Catsville60 <sanrav3000@gmail.com>
2019-03-26 14:54:51 +0700

    Убрал host, это ускорило процесс, отформатировал красиво вывод инфы от РДАП и об ошибках, сделал вывод времени затраченного на выполнение скрипта, TODO

--- test.py
+++ test.py
@@ -1,15 +1,15 @@
 # -*- coding: utf-8 -*-
-import sys
 import socket
 import xlrd
 from ipwhois import IPWhois
 import ipwhois
+import signal
+
+import time
 from pprint import pprint
-from PyQt5.QtWidgets import QWidget, QPushButton, QApplication
-from PyQt5.QtCore import QCoreApplication
 
 
-rb = xlrd.open_workbook('C:/Users/Пользователь/Desktop/exel.xls',
+rb = xlrd.open_workbook('C:/Users/Пользователь/PycharmProjects/untitled1/exel.xls',
                         formatting_info=True)
 sheet = rb.sheet_by_index(0)
 vals = [sheet.row_values(rownum) for rownum in range(sheet.nrows)]
@@ -21,45 +21,52 @@ x = 0
 y = 11
 x1 = 1
 y1 = 0
-
-
+s = socket.socket()
 
 while y <= r_count:
-    try:
-        try:
+        if int((vals[y])[2]) > 1.0:
+            try:
+                try:
+                    ais = socket.gethostbyaddr((vals[y])[x])
 
-            ais = socket.gethostbyaddr((vals[y])[x])
+                    w = IPWhois((vals[y])[x])
+                    res = w.lookup_rdap(retry_count=0)
+                    resp = res.get('objects')
+                    resp1 = list(resp.keys())
+                    resp2 = resp1[0]
+                    resp3 = resp.get(resp2)
+                    resp4 = resp3.get('contact')
+                    resp5 = resp4.get('name')
 
-            w = IPWhois((vals[y])[x])
-            res = w.lookup_rdap(retry_count=0)
-            resp = res.get('objects')
-            resp1 = list(resp.keys())
-            resp2 = resp1[0]
-            resp3 = resp.get(resp2)
-            resp4 = resp3.get('contact')
-            resp5 = resp4.get('name')
+                    print('Host: ', ais[0], ' Owner: ', resp5)
 
-            print('Host: ', ais[0], ' Owner: ', resp5)
+                    y = y + 1
 
-            y = y + 1
+                except socket.herror:
 
-        except socket.herror:
+                    w = IPWhois((vals[y])[x])
+                    ress = w.lookup_rdap(retry_count=0)
+                    respp = ress.get('objects')
+                    respp1 = list(respp.keys())
+                    respp2 = respp1[0]
+                    respp3 = respp.get(respp2)
+                    respp4 = respp3.get('contact')
+                    respp5 = respp4.get('name')
 
-            w = IPWhois((vals[y])[x])
-            ress = w.lookup_rdap(retry_count=0)
-            respp = ress.get('objects')
-            respp1 = list(respp.keys())
-            respp2 = respp1[0]
-            respp3 = respp.get(respp2)
-            respp4 = respp3.get('contact')
-            respp5 = respp4.get('name')
+                    print('Host: Not found! Owner: ', respp5)
 
-            print('Host: Not found! Owner: ', respp5)
+                    y = y + 1
 
-            y = y + 1
+            except ipwhois.exceptions.IPDefinedError as e:
 
-    except ipwhois.exceptions.IPDefinedError as e:
+                print('%s' % e)
 
-        print(' - ', '%s' % e)
+                y = y + 1
+        else:
+            break
 
-        y = y + 1
+
+
+
+
+
--- testexel.py
+++ testexel.py
@@ -1,15 +1,12 @@
 # -*- coding: utf-8 -*-
-import sys
-import socket
 import xlrd
 from ipwhois import IPWhois
 import ipwhois
-import pprint
-from PyQt5.QtWidgets import QWidget, QPushButton, QApplication
-from PyQt5.QtCore import QCoreApplication
+import time
 
-
-rb = xlrd.open_workbook('C:/Users/Пользователь/Desktop/Статистика. Васильченко. 21-22.03.2019.xls', formatting_info=True)
+start_time = time.time()
+rb = xlrd.open_workbook('C:/Users/Пользователь/PycharmProjects/untitled1/exel.xls',
+                        formatting_info=True)
 sheet = rb.sheet_by_index(0)
 vals = [sheet.row_values(rownum) for rownum in range(sheet.nrows)]
 
@@ -21,33 +18,29 @@ y = 11
 x1 = 1
 y1 = 0
 
-while y <= r_count:
+while int((vals[y])[2]) >= 1.0:
+
     try:
-        try:
-            socket.setdefaulttimeout(1)
-            ais = socket.gethostbyaddr((vals[y])[x])
+        w = IPWhois((vals[y])[x])
+        res = w.lookup_rdap(retry_count=0)
+        resp = res.get('objects')
+        resp1 = list(resp.keys())
+        resp2 = resp1[0]
+        resp3 = resp.get(resp2)
+        resp4 = resp3.get('contact')
+        resp5 = resp4.get('name')
 
-            w = IPWhois((vals[y])[x])
-            res = w.lookup_whois()
-            resp = res.get('nets')
-            resp1 = resp[0]
-            resp2 = resp1.get('description')
+        print('Owner: ', resp5)
 
-            print('Host: ', ais[0], ' Owner: ', resp2)
-
-            y = y + 1
-        except socket.herror:
-            w = IPWhois((vals[y])[x])
-            res = w.lookup_whois()
-            resp = res.get('nets')
-            resp1 = resp[0]
-            resp2 = resp1.get('description')
-
-            print('Host: Not found! Owner: ', resp2)
-
-            y = y + 1
-    except ipwhois.exceptions.IPDefinedError as e:
-        err = '%s' % e
-        print(' - ', err)
         y = y + 1
 
+    except ipwhois.exceptions.IPDefinedError as e:
+
+        print('Error: ', '%s' % e)
+
+        y = y + 1
+
+print("--- %s seconds ---" % (time.time() - start_time))
+
+# TODO Прпробовать перепелить код на перл под питон
+# TODO Разобраться с получением инфы о посещениях по ip
