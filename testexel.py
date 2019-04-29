508d6b57478568eb120bd282f36464895672dfe5
Catsville60 <sanrav3000@gmail.com>
2019-04-29 10:50:49 +0700

    Remake some function

--- testexel.py
+++ testexel.py
@@ -4,23 +4,28 @@ from ipwhois import IPWhois
 import ipwhois
 import time
 
-start_time = time.time()
-rb = xlrd.open_workbook('C:/Users/Пользователь/PycharmProjects/exwork/exel.xls',
-                        formatting_info=True)
-sheet = rb.sheet_by_index(0)
-vals = [sheet.row_values(rownum) for rownum in range(sheet.nrows)]
-
-r_count = sheet.nrows
-print('Row count: ', r_count)
-
+p = 0
 x = 0
 y = 11
 x1 = 1
 y1 = 0
 
-while int((vals[y])[2]) >= 1.0:
+start_time = time.time()
+
+rb = xlrd.open_workbook('C:/Users/Пользователь/PycharmProjects/exwork/exew.xls',
+                        formatting_info=True)
+maxsheets = rb.nsheets
+print('Sheets count: ', maxsheets)
+
+
+
+while int(maxsheets) >= p:
+    sheet = rb.sheet_by_index(p)
+    vals = [sheet.row_values(rownum) for rownum in range(sheet.nrows)]
+    r_count = sheet.nrows
 
     try:
+
         w = IPWhois((vals[y])[x])
         res = w.lookup_rdap(retry_count=0)
         resp = res.get('objects')
@@ -33,14 +38,29 @@ while int((vals[y])[2]) >= 1.0:
         print('Owner: ', resp5)
 
         y = y + 1
+        if int((vals[y])[2]) <= 1.0:
+
+            p = p + 1
+            y = 11
+            x = 0
+            print("--- %s seconds ---" % (time.time() - start_time), p)
+            continue
+        if p == int(maxsheets):
+            break
 
     except ipwhois.exceptions.IPDefinedError as e:
 
         print('Error: ', '%s' % e)
 
         y = y + 1
+        if int((vals[y])[2]) >= 1.0:
+            p = p + 1
+            print("--- %s seconds ---" % (time.time() - start_time), "Sheet: ", p)
+            continue
+        if p == int(maxsheets):
+            break
 
-print("--- %s seconds ---" % (time.time() - start_time))
+print("--- %s seconds ---" % (time.time() - start_time), "Sheet: ", p)
 
 # TODO Прпробовать перепелить код на перл под питон
 # TODO Разобраться с получением инфы о посещениях по ip
